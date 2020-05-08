551e1bc6a5d04002aba4893beee1d965e1b5cd78
Lendruk <fenixeye3@gmail.com>
2020-05-08 16:44:49 +0100

    dovenv and database connection

--- backend/app/app.ts
+++ backend/app/app.ts
@@ -5,12 +5,17 @@ import cors from 'cors';
 import dbConnection from './utils/database';
 import { ErrorManager, errors } from './utils/errors';
 import { RouteAggregator } from './controllers';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Connect DB
+dbConnection().then(() => console.log("Database Connected..."));
 
 const mainRouter = new RouteAggregator(app);
 
--- backend/app/utils/database.ts
+++ backend/app/utils/database.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 mongoose.set("debug", true);
 
-const dbConnection = null;
+const dbConnection = async() => await mongoose.connect("mongodb+srv://ams-sii-8orgd.mongodb.net/test",
+{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, auth: { user: process.env.DATABASE_USERNAME as string, password: process.env.DATABASE_PASSWORD as string }});
 
 export { mongoose };
 export default dbConnection;
