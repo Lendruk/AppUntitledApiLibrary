306c04d8b2db07e0e3ecc467461096858b8f65c1
Lendruk <fenixeye3@gmail.com>
2020-06-13 02:07:53 +0100

    wip task

--- frontend/src/components/Task/index.js
+++ frontend/src/components/Task/index.js
@@ -12,7 +12,18 @@ export class Task extends React.Component {
     render() {
         const { task } = this.props;
         return (
-        <Styles.Task>{task.title}</Styles.Task>
+        <Styles.Task>
+            <Styles.Poly >
+                <span className="moon-users" /> 
+                <svg width="50" height="50">
+                    <polygon points="0,0 50,0 0,50" />
+                </svg>
+            </Styles.Poly>
+            <Styles.TaskTitle>{task.title}</Styles.TaskTitle>    
+            <Styles.Tags>
+                <Styles.Tag>test</Styles.Tag>
+            </Styles.Tags>
+        </Styles.Task>
         )
     }
 
--- frontend/src/components/Task/styles.js
+++ frontend/src/components/Task/styles.js
@@ -1,5 +1,43 @@
 import styled from 'styled-components';
 
 export const Task = styled.div`
-    
+    border: 1px solid black;
+    border-radius: 3px;
+    border-left: 7px solid red;
+    border-top: 7px solid red;
+    min-height: 120px;
+    position: relative;
+
+    cursor: pointer;
+`;
+
+export const Poly = styled.div`
+    height: 20px;
+    width: 20px;
+    position: relative;
+    .moon-users {
+        position: absolute;
+        top: 5px;
+        left: 5px;
+    }
+    background: red;
+    svg {
+        fill:red;
+    }
+`;
+
+export const TaskTitle = styled.div`
+    margin-left: 40px;
+`;
+
+export const Tags = styled.div`
+    padding: 0px 20px;
+    display: flex;  
+`;
+
+export const Tag = styled.div`
+    background: blue;
+    color:white;
+    padding: 3px 10px;
+    border-radius: 15px;
 `;
