034c65912eb967d3950ccf15cf0888fbbbd00853
tiagocardoso95 <tcardoso13@gmail.com>
2020-06-23 15:57:24 +0100

    add logout endpoint

--- backend/app/controllers/auth.ts
+++ backend/app/controllers/auth.ts
@@ -6,6 +6,7 @@ import User from "../models/user";
 import { errors } from "../utils/errors";
 import { LoginService } from "../services/LoginService";
 import { Inject } from "../lib/decorators/Inject";
+import Token from "../models/token";
 
 @Controller("/auth")
 export class AuthController extends BaseController {
@@ -19,6 +20,16 @@ export class AuthController extends BaseController {
 
         if (!email || !password) throw errors.REQUIRED_FIELDS_EMPTY;
 
-        return { status: 'LOGIN_SUCCESSFULL', ...await this._loginService.login(email, password) };
+        return { status: 'LOGIN_SUCCESSFUL', ...await this._loginService.login(email, password) };
+    }
+
+    @Post("/logout")
+    public async logout(req: Request) {
+        const { authorization } = req.headers;
+        if (!authorization) throw errors.NO_TOKEN;
+        const splitToken: string[] = authorization.split(' ');
+        await Token.findOneAndDelete({ authToken: splitToken[1] }).lean();
+
+        return { status: 'LOGOUT_SUCCESSFUL', message: 'Logged out successfully' }
     }
 }
